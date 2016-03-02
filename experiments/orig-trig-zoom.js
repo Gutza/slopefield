5fc13a69653f11e53a9b40b5d27e5668d3510eea
Bogdan Stancescu <gutza@moongate.ro>
2016-03-02 22:28:24 +0200

    Better zoom + interactive slope field resolution

--- experiments/orig-trig-zoom.js
+++ experiments/orig-trig-zoom.js
@@ -35,6 +35,8 @@ var drawWindow = new ViewWindow(
 	new Point(30, 30)
 );
 
+var slopeFieldSize = 0.5;
+
 var drawMode = "diamonds";
 
 function slopeField(p)
@@ -57,7 +59,8 @@ function slopeField(p)
 		//This is where you should define the first-order differential equation
 		//Assumes it can be expressed as y' = f(x, y(x))
 		var _slopeAtPoint = function(x, y) {
-			return Math.cos(x/50 + xFuncOffset/3000) * Math.sin(x*y/94+xFuncOffset/60);
+			return x/y;
+			//return Math.cos(x/50 + xFuncOffset/3000) * Math.sin(x*y/94+xFuncOffset/60);
 			//return -(x-xFuncOffset)/y;
 		};
 		
@@ -84,8 +87,6 @@ function slopeField(p)
 				return  p.height-((y-drawWindow.pMin.y)*yScale+ yOffset);
 			};
 
-			var slopeFieldSize = 1.0;
-
 			p.background(58, 66, 74);
 			
 			// Draw slope field
@@ -96,8 +97,8 @@ function slopeField(p)
 				p.strokeWeight(1);
 				p.stroke(200);
 			}
-			for (var x = drawWindow.pMin.x; x <= drawWindow.pMax.x + 1; x++) {
-				for (var y = drawWindow.pMin.y; y <= drawWindow.pMax.y + 1; y++) {
+			for (var x = drawWindow.pMin.x; x <= drawWindow.pMax.x + 1; x += slopeFieldSize) {
+				for (var y = drawWindow.pMin.y; y <= drawWindow.pMax.y + 1; y += slopeFieldSize) {
 					var slope = slopeAtPoint(x, y);
 					var ang = Math.atan(slope);
 					var angPerp = ang + Math.PI / 2;
@@ -135,99 +136,109 @@ function slopeField(p)
 	}
 }
 
-	var xMin = 5;
-	var yMin = 5;
+var canvas = document.getElementById("canvas");
+var processingInstance = new Processing(canvas, slopeField);
+processingInstance.draw();
 
-	var xMax = 30;
-	var yMax = 30;
- 
-	var canvas = document.getElementById("canvas");
-	var processingInstance = new Processing(canvas, slopeField);
+canvas.addEventListener('mousewheel', EvScroll, false);
+canvas.addEventListener("DOMMouseScroll", EvScroll, false);
+canvas.addEventListener("mouseup", EvUp, false);
+canvas.addEventListener("mouseout", EvUp, false);
+canvas.addEventListener("mousemove", EvMove, false);
+canvas.addEventListener("mousedown", EvDown, false);
+canvas.addEventListener("mouseenter", EvEnter, false);
+canvas.addEventListener("mouseleave", EvLeave, false);
+
+
+function EvScroll(e)
+{
+	//var e = window.event || e;
+	var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+	e.preventDefault = true;
+	
+	if (e.shiftKey) {
+		slopeFieldSize *= 1 - delta/10;
+		processingInstance.draw();
+		return false;
+	}
+	
+	//$(canvas).width($(canvas).width() + delta * 6);
+	if (delta > 0 && drawWindow.pMax.x - drawWindow.pMin.x < 0.001)
+		return false;
+
+	var midX = (drawWindow.pMax.x + drawWindow.pMin.x) / 2;
+	var midY = (drawWindow.pMax.y + drawWindow.pMin.y) / 2;
+	
+	var sizeFactor = 1 - delta/10;
+	var newWidth = drawWindow.width() * sizeFactor;
+	var newHeight = drawWindow.height() * sizeFactor;
+	
+	drawWindow.pMin.x = midX - newWidth/2;
+	drawWindow.pMin.y = midY - newHeight/2; 
+	drawWindow.pMax.x = midX + newWidth/2;
+	drawWindow.pMax.y = midY + newHeight/2;
 	processingInstance.draw();
+	//canvas.style.width = (canvas.width + delta*2) + "px";
+	//alert(delta);
 	
-	canvas.addEventListener('mousewheel', EvLis, false);
-	canvas.addEventListener("DOMMouseScroll", EvLis, false);
-	canvas.addEventListener("mouseup", EvUp, false);
-	canvas.addEventListener("mouseout", EvUp, false);
-	canvas.addEventListener("mousemove", EvMove, false);
-	canvas.addEventListener("mousedown", EvDown, false);
+	
+	return false; 
+}
 
-	function EvLis(e)
-	{
-		//var e = window.event || e;
-		var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-		e.preventDefault = true;
-		//$(canvas).width($(canvas).width() + delta * 6);
-		if (delta > 0 && drawWindow.pMax.x - drawWindow.pMin.x < 4)
-			return false;
+var dragging = false;
+var dragStartMouse = {};
+var dragStartWindow;
 
-		drawWindow.pMin.x += delta;
-		drawWindow.pMin.y += delta; 
-		drawWindow.pMax.x -= delta;
-		drawWindow.pMax.y -= delta;
-		processingInstance.draw();
-		//canvas.style.width = (canvas.width + delta*2) + "px";
-		//alert(delta);
-		
-		
-		return false; 
-	}
-	
-	var dragging = false;
-	var dragStartMouse = {};
-	var dragStartWindow;
-	
-	function EvDown(e)
-	{
-		dragging = true;
-		dragStartMouse = MousePos(e);
-		dragStartWindow = drawWindow.clone();
-		drawMode = "lines";
-		processingInstance.draw();
-	}
-	
-	function EvMove(e)
-	{
-		if (!dragging)
-			return;
-		
-		var stat = "";
-		
-		if (e.ctrlKey)
-			stat += "+CTRL";
-		if (e.shiftKey)
-			stat += "+SHIFT";
-		
-		console.log("Mod: "+stat);
-		
-		var currMousePos = MousePos(e);
-		var deltaFactor = canvas.width / dragStartWindow.width();
-		var xDelta = (currMousePos.x - dragStartMouse.x) / deltaFactor;
-		var yDelta = (currMousePos.y - dragStartMouse.y) / deltaFactor;
-		
-		drawWindow.pMin.x = dragStartWindow.pMin.x - xDelta;
-		drawWindow.pMax.x = dragStartWindow.pMax.x - xDelta;
+function EvDown(e)
+{
+	dragging = true;
+	dragStartMouse = MousePos(e);
+	dragStartWindow = drawWindow.clone();
+	drawMode = "lines";
+	processingInstance.draw();
+}
 
-		drawWindow.pMin.y = dragStartWindow.pMin.y + yDelta;
-		drawWindow.pMax.y = dragStartWindow.pMax.y + yDelta;
-		
-		//console.log("xDelta = "+xDelta + "; dSW.pMin.x = "+dragStartWindow.pMin.x);
-		
-		processingInstance.draw();
-	}
+function EvMove(e)
+{
+	if (!dragging)
+		return;
 	
-	function EvUp(e)
-	{
-		dragging = false;
-		drawMode = "diamonds";
-		processingInstance.draw();
-	}
+	var currMousePos = MousePos(e);
+	var deltaFactor = canvas.width / dragStartWindow.width();
+	var xDelta = (currMousePos.x - dragStartMouse.x) / deltaFactor;
+	var yDelta = (currMousePos.y - dragStartMouse.y) / deltaFactor;
 	
-	function MousePos(event)
-	{
-		event = (event ? event : window.event);
-		return new Point(
-			event.pageX - canvas.offsetLeft,
-			event.pageY - canvas.offsetTop
-		);
-	}
+	drawWindow.pMin.x = dragStartWindow.pMin.x - xDelta;
+	drawWindow.pMax.x = dragStartWindow.pMax.x - xDelta;
+
+	drawWindow.pMin.y = dragStartWindow.pMin.y + yDelta;
+	drawWindow.pMax.y = dragStartWindow.pMax.y + yDelta;
+	
+	processingInstance.draw();
+}
+
+function EvUp(e)
+{
+	dragging = false;
+	drawMode = "diamonds";
+	processingInstance.draw();
+}
+
+function MousePos(event)
+{
+	event = (event ? event : window.event);
+	return new Point(
+		event.pageX - canvas.offsetLeft,
+		event.pageY - canvas.offsetTop
+	);
+}
+
+function EvEnter(e)
+{
+	$('body').css('overflow', 'hidden');
+}
+
+function EvLeave(e)
+{
+	$('body').css('overflow', 'auto');
+}
